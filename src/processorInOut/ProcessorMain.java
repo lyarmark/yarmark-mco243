f6342b94715119e97df9fd4e876f67f11bae7444
lyarmark <student1@student.touro.edu>
2016-03-10 21:12:01 -0500

    minor change

--- src/processorInOut/ProcessorMain.java
+++ src/processorInOut/ProcessorMain.java
@@ -3,7 +3,9 @@ package processorInOut;
 import java.io.IOException;
 
 public class ProcessorMain {
-
+//run from cmd:
+	//java projectName.ClassName <fileLocation.fileName>
+	//ex: java processorInOut.ProcessorMain <../mach.in>
 	public static void main(String[] args) throws IOException {
 		Processor p = new Processor();
 		p.readInFile();
